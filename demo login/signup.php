6af651c2856fdaa459a9b7b496ccb7afa87b0146
Kelly257 <128812031+Kelly257@users.noreply.github.com>
2024-04-19 22:54:34 +0200

    ok
    ok

---
+++ demo login/signup.php
@@ -0,0 +1,40 @@
+<?php
+include_once("connection.php");
+$result=0;
+$user= $_POST['username'];
+$pass1= $_POST['password1'];
+$pass2= $_POST['password2'];
+
+if ($pass1!=$pass2)// Zijn de wachtwoorden gelijk
+{
+    $melding= "Beide wachtwoorden zijn niet identiek";
+    header("Location: signupForm.php?melding=$melding");
+
+}
+
+else //Bestaat de user al
+{
+    $result = $conn -> query("SELECT * FROM users WHERE gebruikersnaam='$user' " );
+
+    if($result->num_rows==0)
+    {
+        $sql = "INSERT INTO users (gebruikersnaam, paswoord) VALUES ('$user','$pass1')";
+
+        if ($conn ->query($sql)=== TRUE ){
+            $melding="Account is toegevoegd";
+            session_start();
+            $_SESSION["user"]==$user;
+
+        }
+        else{
+            $melding="Error: ". $sql . "<br>" . $conn->error;
+
+        }
+    }
+    else{
+        $melding= "De gebruikersnaam bestaat al";
+
+    }
+    header("Location: signupForm.php?melding=$melding");
+    $conn->close();
+}
